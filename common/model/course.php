d7bee86ea80ab166eab7996750fef36d7922a15f
haozelong <1580380768@qq.com>
2020-09-25 17:47:04 +0800

    copy

---
+++ common/model/course.php
@@ -0,0 +1,48 @@
+<?php
+namespace app\common\model;
+use think\Model;
+/**
+ * 班级
+ */
+class Course extends Model
+{
+    private $Teacher;
+
+    /**
+     * 获取对应的教师（辅导员）信息
+     * @return Teacher 教师
+     * @author <panjie@yunzhiclub.com> http://www.mengyunzhi.com
+     */
+    public function Teacher()
+    {
+        return $this->belongsTo('teacher');
+    }
+    public function Students()
+    {
+        return $this->belongsToMany('Student');
+    }
+    public function CourseStudents()
+    {
+        return $this->hasMany('CourseStudent');
+    }
+    public function getIsChecked(Student &$Student)
+    {
+        // 取课程ID
+        $courseId = (int)$this->id;
+        $studentId = (int)$Student->id;
+
+        // 定制查询条件
+        $map = array();
+        $map['student_id'] = $studentId;
+        $map['course_id'] = $courseId;
+
+        // 从关联表中取信息
+        $CourseStudent = CourseStudent::get($map);
+        if (is_null($CourseStudent)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
---
+++ index/controller/StudentController.php
@@ -0,0 +1,134 @@
+<?php
+namespace app\index\controller;
+use app\common\model\Student;
+use think\Request;
+use app\common\model\Klass;
+use app\index\controller\Login;
+class StudentController extends IndexController
+{
+	public function index()
+	{
+		$students=Student::paginate(5);
+		$this->assign('students',$students);
+		return $this->fetch();
+	}
+
+	public function add()
+	{
+		$Student=new Student;
+		$Student->sex=0;
+		$Student->email='';
+		$Student->num='';
+		$Student->name='';
+		$Student->klass_id=1;
+		$Student->id=0;
+
+		$this->assign('Student',$Student);
+
+		return $this->fetch('edit');
+	}
+
+	public function edit()
+	{
+		$id=Request::instance()->param('id/d');
+
+		//判断是否存在为此id的记录
+
+		if(is_null($Student=Student::get($id)))
+		{
+			return $this->error('未找到ID为'.id.'的记录');
+		}
+
+		//取出班级列表
+		$this->assign('Student',$Student);
+		return $this->fetch();
+	}
+
+	public function save()
+	{
+		//实例化请求信息
+    	$Student=new Student;
+		//新增数据
+		if (!$this->saveStudent($Student)) {
+            return $this->error('操作失败' . $Student->getError());
+        }
+    
+        // 成功跳转至index触发器
+        return $this->success('操作成功', url('index'));
+	}
+
+	public function update()
+	{
+		//实例化请求
+		$id=Request::instance()->post('id/d');
+		$Student=Student::get($id);
+
+		if(!is_null($Student))
+        {
+            if(!$this->saveStudent($Student,true))
+            {
+                return $this->error('操作失败'.$Student->getError());
+            }  }else {
+                return $this->error('当前操作的记录不存在');
+                
+            }
+       return $this->success('操作成功',url('index'));
+
+	}
+
+	private function saveStudent(Student &$Student,$isUpdate= false)
+    {
+        //写入要传入的数据
+        $Student->name=Request::instance()->post('name');
+
+        if(!$isUpdate)
+        {
+            $Student->klass_id=Request::instance()->post('klass_id/d');
+            $Student->num=Request::instance()->post('num/d');
+            $Student->sex=Request::instance()->post('sex/d');
+            $Student->email=Request::instance()->post('email');
+        }
+
+        //更新并保存数据
+        return $Student->validate(true)->save();
+    }
+
+
+    public function delete()
+    {
+        try {
+            // 获取get数据
+            $Request = Request::instance();
+            $id = Request::instance()->param('id/d');
+            
+            // 判断是否成功接收
+            if (0 === $id) {
+                throw new \Exception("未获取到ID信息", 1);
+            }
+
+            // 获取要删除的对象
+            $Student = Student::get($id);
+
+            // 要删除的对象存在
+            if (is_null($Student)) {
+                throw new \Exception('不存在id为' . $id . '的学生，删除失败', 1);
+            }
+
+            // 删除对象
+            if (!$Student->delete()) {
+                $message = '删除失败:' . $Teacher->getError();
+            }
+            }
+        //  获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
+        catch(\think\Exception\HttpResponseException $e){
+            throw $e;
+            //获取到正常的异常时，输出异常
+            
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+	// 进行跳转
+	return $this->success('删除成功', $Request->header('referer')); 
+        
+    }
+}
