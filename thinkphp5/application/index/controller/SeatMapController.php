2e92f3eb31cdb30c536b981d17bc70ac11e15eb5
chenliting <2567826278@qq.com>
2020-12-23 23:29:17 +0800

    初步动态生成座位图

--- thinkphp5/application/index/controller/SeatMapController.php
+++ thinkphp5/application/index/controller/SeatMapController.php
@@ -10,6 +10,7 @@ use app\common\model\Seat;
 class SeatMapController extends Controller{
 	
 	public function index(){
+
 		$id = Request::instance()->get('id\d');
 		$course_id = Request::instance()->param('course_id');
 		// 实例化课程
@@ -17,17 +18,41 @@ class SeatMapController extends Controller{
 
 		$this->assign('Course',$Course);
 		return $this->fetch();
+
 	}
 	public function add(){
+
 		return $this->fetch();
+
 	}
 	public function edit(){
+		$id = Request::instance()->param('id');
+		$seat = new Seat;
+		$seat = Seat::where('seat_map_id','=',$id)->select();
+		$this->assign('seats',$seat);
+		$SeatMap = new SeatMap;
+		$SeatMap = SeatMap::get($id);
+		$this->assign('SeatMap',$SeatMap);
 		return $this->fetch();
+	}
+	public function save(){
+		$SeatMap = new SeatMap;
+		$SeatMap->x_map = Request::instance()->post('x_map');
+		$SeatMap->y_map = Request::instance()->post('y_map');
+		if(!$SeatMap->save()){
+			return $this->error('保存信息错误'.$SeatMap->getError());
+		}
+		$id = $SeatMap->id;
+		return $this->success('请选择座位和过道',url('Seat/add?id='.$id));
 	}
 	public function template1(){
+
 		return $this->fetch();
+
 	}
 	public function template2(){
+
 		return  $this->fetch();
+
 	}
 }
