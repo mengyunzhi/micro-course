2c9a32b05ea0d0305c6accbcbe32d789dcb083d2
chenliting <2567826278@qq.com>
2021-01-04 11:01:45 +0800

    文件上传

--- thinkphp5/application/index/controller/TestController.php
+++ thinkphp5/application/index/controller/TestController.php
@@ -20,35 +20,20 @@ class TestController extends IndexController
 		return $this->fetch();
 	}
 	public function file1() {
-		if(isset($_POST["submit"])) {
-                $url='localhost';
-                $username='root';
-                $password='';
-                $conn=mysqli_connect($url,$username,$password,"location");
-          if(!$conn){
-          die('Could not Connect My Sql:' .mysqli_error());
-		  }
-          $file = $_FILES['file']['tmp_name'];
-          $handle = fopen($file, "r");
-          $c = 0;
-          while(($filesop = fgetcsv($handle, 1000, ",")) !== false) {
-          	$fname = $filesop[0];
-          	$lname = $filesop[1];
-          	$sql = "insert into excel(fname,lname) values ('$fname','$lname')";
-          	$stmt = mysqli_prepare($conn,$sql);
-          	mysqli_stmt_execute($stmt);
+    $uploaddir = 'd:/data/';
+    $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
-         	$c = $c + 1;
-           }
 
-            if($sql){
-               echo "sucess";
-             } 
-		 	else
-		 	{
-            	echo "Sorry! Unable to impo.";
-          	}
+    echo '<pre>';
+    if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+        echo "File is valid, and was successfully uploaded.\n";
+    } else {
+        echo "Possible file upload attack!\n";
+    }
 
-		}
-	}
+    echo 'Here is some more debugging info:';
+    print_r($_FILES);
+
+    print "</pre>";
+  }
 }
