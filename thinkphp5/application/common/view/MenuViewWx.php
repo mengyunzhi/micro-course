4225ce431009f4b4ec949a208090e0e52ac451e1
haozelong <1580380768@qq.com>
2021-01-11 14:23:35 +0800

    各数据流建立完成

--- thinkphp5/application/common/view/MenuViewWx.php
+++ thinkphp5/application/common/view/MenuViewWx.php
@@ -8,18 +8,18 @@ class MenuViewWx {
 	 public function __construct() {
 		 $courseMenu = new Menu;
 		 $courseMenu->title = '课程查看';
-		 $courseMenu->action = 'course';
-		 $courseMenu->controller = 'WxTeacher';
+		 $courseMenu->action = 'index';
+		 $courseMenu->controller = 'Teacherwx';
 
 		 $courseGradeMenu = new Menu;
 		 $courseGradeMenu->title = '上课表现';
 		 $courseGradeMenu->action = 'wxaod';
-		 $courseGradeMenu->controller = 'WxTeacher';
+		 $courseGradeMenu->controller = 'Teacherwx';
 
 		 $signMenu = new Menu;
 		 $signMenu->title = '签到修改';
 		 $signMenu->action = 'signChange';
-		 $signMenu->controller = 'WxTeacher';
+		 $signMenu->controller = 'Teacherwx';
 
 		 $this->menus = [$courseMenu, $courseGradeMenu, $signMenu];
 		 $this->viewHtml = view('index/menu', 
