f8acd681c32add05f763c9616063b646912d835c
haozelong <1580380768@qq.com>
2021-01-28 21:06:52 +0800

    更新修改

--- thinkphp5/application/common/model/Term.php
+++ thinkphp5/application/common/model/Term.php
@@ -1,15 +1,11 @@
 <?php
 namespace app\common\model;
 use think\Model;
-use traits\model\SoftDelete;
 
 class Term extends Model{
     static $Term_id;
 	protected $dateFormat = 'Y年m月d日';    // 日期格式
 	protected $state = 0;
-    use SoftDelete;
-    protected static $deleteTime = 'delete_time';
-    protected $defaultSoftDelete = 0;
 
    /**
      * 获取要显示的创建时间
